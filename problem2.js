518321a5975d10860409206be26f5363aa939280
Imtiaz AL Kabir <imtiazalkabir@gmail.com>
2025-08-11 17:16:42 +0600

    task  done

--- problem2.js
+++ problem2.js
@@ -103,7 +103,7 @@ function findAveragePhonePrice(phones) {
   for (const phone of phones) {
     average += phone.price;
   }
-  return average
+  return average;
 }
 console.log(findAveragePhonePrice(phones));
 
@@ -120,3 +120,16 @@ For each employee their current salary is calculated by multiplying yearly incre
 ```
 
 */
+
+const employees = [
+  { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+  { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+  { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+  { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+];
+
+let totalSalary = 0;
+for (const employee of employees) {
+  totalSalary += employee.starting + employee.experience * employee.increment;
+}
+console.log(totalSalary);
